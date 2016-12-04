f5054dbb5932199c69da296dbb19b0ac5d2b0e60
alx <37true@gmail.com>
2016-12-04 19:43:06 +0700

    first

---
+++ app/src/main/java/tk/alltrue/criminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package tk.alltrue.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ya on 04.12.16.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+}
