fe922ec396f117a76b9b92edc330e8855a016a36
alx <37true@gmail.com>
2016-12-28 19:57:39 +0700

    first

--- app/src/main/java/tk/alltrue/criminalintent/Crime.java
+++ app/src/main/java/tk/alltrue/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package tk.alltrue.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,28 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTIitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
