50807861210becb525cb26052dc034196097611d
ya <7true@mail.ru>
2017-01-06 18:25:31 +0700

    ch 9

---
+++ app/src/main/java/tk/alltrue/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package tk.alltrue.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ya on 06.01.17.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
--- app/src/main/java/tk/alltrue/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/tk/alltrue/criminalintent/SingleFragmentActivity.java
@@ -1,8 +1,30 @@
 package tk.alltrue.criminalintent;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
 /**
  * Created by ya on 06.01.17.
  */
 
-public class SingleFragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer) ;
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+
+    }
 }
