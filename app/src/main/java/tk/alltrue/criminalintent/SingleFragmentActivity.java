d98c5c13546f0740cc65ab7a9e97337a3c422428
ya <7true@mail.ru>
2017-06-02 18:41:14 +0700

    chapter 17

--- app/src/main/java/tk/alltrue/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/tk/alltrue/criminalintent/SingleFragmentActivity.java
@@ -3,16 +3,17 @@ package tk.alltrue.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by ya on 06.01.17.
  */
-
+/*
 public abstract class SingleFragmentActivity extends AppCompatActivity {
+
     protected abstract Fragment createFragment();
+
     @LayoutRes
     protected int getLayoutResId() {
         return R.layout.activity_fragment;
@@ -22,13 +23,39 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResId());
+
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer) ;
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
             fragment = createFragment();
             fm.beginTransaction()
-                    .add(R.id.fragmentContainer, fragment)
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}*/
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container) ;
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
                     .commit();
         }
 
