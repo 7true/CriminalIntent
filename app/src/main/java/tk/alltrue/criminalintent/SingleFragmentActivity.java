383be180d8d67ffc2df9cb652b25796c5a1a0693
alx <37true@gmail.com>
2017-06-02 07:28:10 +0700

    Chapter 17

--- app/src/main/java/tk/alltrue/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/tk/alltrue/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package tk.alltrue.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -12,6 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
